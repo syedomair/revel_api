b579d642b9d1ab691c282ae461b1c01e2615e67b
Syed Omair <syedkhalidomair@gmail.com>
2017-02-17 13:15:17 +0500

    DATABASE URL added

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -2,8 +2,7 @@ package controllers
 
 import (
 	"database/sql"
-	"fmt"
-
+	//"fmt"
 	"github.com/revel/revel"
 )
 
@@ -12,10 +11,15 @@ var db *sql.DB
 
 //InitDB initializes DB connection
 func InitDB() {
-	connstring := fmt.Sprintf("user=%s password='%s' dbname=%s sslmode=disable", "khalid", "khalid", "project1")
+	//connstring := fmt.Sprintf("user=%s password='%s' dbname=%s sslmode=disable", 
+        //    revel.Config.StringDefault("db.user_name", ""), 
+        //    revel.Config.StringDefault("db.password", ""), 
+        //    revel.Config.StringDefault("db.db_name", ""))
 
 	var err error
-	db, err = sql.Open("postgres", connstring)
+	//db, err = sql.Open("postgres", connstring)
+        //db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+        db, err = sql.Open("postgres", revel.Config.StringDefault("db.spec", "") )
 	if err != nil {
 		revel.INFO.Println("DB Error", err)
 	}
